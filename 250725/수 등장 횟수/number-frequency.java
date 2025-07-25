e81d93aa7aaf7d43bad2f5b5f7cda5b13b4b8806
pbh914 <akdl4045@naver.com>
2025-07-25 21:35:27 +0900

    Title: 수 등장 횟수, Time: 1536ms, Memory: 55MB, Status: Wrong Answer - Codetree

---
+++ 250725/수 등장 횟수/number-frequency.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+
+        HashMap<Integer,Integer> countMap = new HashMap<>();
+
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            int ie = countMap.getOrDefault(arr[i],0);
+            
+            countMap.put(arr[i],ie+1);
+        }
+        int[] queries = new int[m];
+        for (int i = 0; i < m; i++) {
+            queries[i] = sc.nextInt();
+            if(countMap.get(queries[i]) != null)
+            System.out.print(countMap.get(queries[i])+ " ");
+            else
+            System.out.println(0 + " ");
+        }
+
+    }
+}
